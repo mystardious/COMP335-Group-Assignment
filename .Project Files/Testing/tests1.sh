14374855389995c8f458da343ce872ccdd181eae
mystardious <optimus.maatouk@gmail.com>
2019-04-03 19:23:13 +1100

    Client Simulator Update: 	- Server Information can be saved by the client 	- Server Responses can be read using readline() by adding -n to ./server 	- Login is set to user's computer login name

---
+++ .Project Files/Testing/tests1.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# to kill multiple runaway processes, use 'pkill runaway_process_name'
+configDir="./configs"
+diffLog="stage1diff.log"
+if [ ! -d $configDir ]; then
+	echo "No $configDir found!"
+	exit
+fi
+
+if [ -f $configDir/$diffLog ]; then
+	rm $configDir/*.log
+fi
+
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 your_client [-n]"
+	exit
+fi
+
+if [ ! -f $1 ]; then
+	echo "No $1 found!"
+	echo "Usage: $0 your_client [newline]"
+	exit
+fi
+
+if [ -f $configDir/$diffLog ]; then
+	rm $configDir/$diffLog
+fi
+
+trap "kill 0" EXIT
+
+for conf in $configDir/*.xml; do
+	echo "$conf"
+	echo "running the reference implementation (./ds-client)..."
+	sleep 1
+	if [ -z $2 ]; then
+		./ds-server -c $conf -v brief > $conf.ref.log&
+		sleep 1
+		./ds-client
+	else
+		./ds-server -c $conf -v brief -n > $conf.ref.log&
+		sleep 1
+		./ds-client -n
+	fi
+	
+	echo "running your implementation ($1)..."
+	sleep 2
+	if [ -z $2 ]; then
+		./ds-server -c $conf -v brief > $conf.your.log&
+	else
+		./ds-server -c $conf -v brief -n > $conf.your.log&
+	fi
+	sleep 1
+	if [ -f $1.class ]; then
+		java $1
+	else
+		./$1
+	fi
+	sleep 2
+	diff $conf.ref.log $conf.your.log > $configDir/temp.log
+	if [ -s $configDir/temp.log ]; then
+		echo NOT PASSED!
+	else
+		echo PASSED!
+	fi
+	echo ============
+	sleep 1
+	cat $configDir/temp.log >> $configDir/$diffLog
+done
+
+echo "testing done! check $configDir/$diffLog"
+
